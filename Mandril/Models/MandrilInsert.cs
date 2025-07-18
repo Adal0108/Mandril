8b4fef1e0cfaacf47559559123de0e99ed81fd1e
adalgif <126955198+adalgif@users.noreply.github.com>
2025-07-18 16:03:19 -0400

    se crea el enpoint put

--- Mandril/Models/MandrilInsert.cs
+++ Mandril/Models/MandrilInsert.cs
@@ -1,10 +1,15 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace MandrilAPI;
 
 public class MandrilInsert
 {
+    [Required]
+    [MaxLength(50)]
     public string Nombre { get; set; }= string.Empty;
-
+    [Required]
+    [MaxLength(50)]
     public string Apellido { get; set; }= string.Empty;
 
 
